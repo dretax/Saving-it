79ff1a0f9678bdb53d95f605f865db5aaa857d59
DreTaX <dretax14@gmail.com>
2013-03-06 12:09:08 +0100

    Saving It 1.6
    Signed-off-by: DreTaX <dretax14@gmail.com>

--- src/me/dretax/SaveIt/Main.java
+++ src/me/dretax/SaveIt/Main.java
@@ -12,6 +12,11 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+/**
+*
+* @author DreTaX
+*/
+
 public class Main extends JavaPlugin {
 	public void onDisable() {
 		super.onDisable();
